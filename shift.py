622cbb94d92957ab02893a89ba61f02893c30aca
David van Erkelens <me@davidvanerkelens.nl>
2018-05-26 16:36:42 +0200

    working on automatic pitch shifting

--- shift.py
+++ shift.py
@@ -1,15 +1,25 @@
 import pretty_midi
+import os
 
-def shift():
-    data = pretty_midi.PrettyMIDI('storage/midifiles/Donna Summer - I Feel Love (version 1).mid')
-    for instrument in data.instruments:
-        # Don't want to shift drum notes
-        if not instrument.is_drum:
-            for note in instrument.notes:
-                note.pitch += 1
+def shift(file):
+    data = pretty_midi.PrettyMIDI('storage/midifiles/' + file)
 
-    data.write('storage/midishifted/love.mid')
+    name, ext = file.split('.', 1)
+
+    if not os.path.exists('storage/midishifted/' + name):
+        os.makedirs('storage/midishifted/' + name)
+
+    data.write('storage/midishifted/' + name + '/' + name + '_0.mid')
+
+    for x in range(1, 12):
+        print(x)
+        for instrument in data.instruments:
+            # Don't want to shift drum notes
+            if not instrument.is_drum:
+                for note in instrument.notes:
+                    note.pitch += 1
+        data.write('storage/midishifted/' + name + '/' + name + '_' + str(x)  + '.mid')
 
 
 if __name__ == '__main__':
-    shift()
+    shift('Donna Summer - I Feel Love (version 1).mid')
